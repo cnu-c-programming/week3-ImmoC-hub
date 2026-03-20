ccfdb38205d97be5d6cfe7756deafd6a48e3380e
seunghyeon <aig0320@naver.com>
2026-03-20 10:13:24 +0900

    3-5

---
+++ code/3-5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char type, int count, ...){
+    va_list ap;
+    va_start(ap,count);
+    switch(type){
+        case 'S':
+            for (int i=0;i<count;i++){
+                printf("%s ",va_arg(ap,char *));
+            }
+            printf("\n");
+            break;
+        case 'C':
+            for (int i=0;i<count;i++){
+                printf("%c",va_arg(ap,int));
+            }
+            printf("\n");
+            break;
+        case 'D':
+            int sum =0;
+            for (int i=0;i<count;i++){
+                sum+=va_arg(ap,int);
+            }
+            printf("%d\n",sum);
+            break;
+    }
+    va_end(ap);
+}
+
+int main() {
+    my_sum('S', 2, "Hello", "World");
+    my_sum('C', 3, 'C','N','U');
+    my_sum('D', 4, 10, 20, 30, 40);
+
+    return 0;
+}
